359ebba6a7d448b06e77c096d3ccc55fdefd6047
Pedro Matias de Araujo <pmdragon@outlook.com>
2017-08-11 17:52:35 -0300

    Start open binary files

--- maycry.py
+++ maycry.py
@@ -20,6 +20,8 @@ parser.add_option("-i", "--input", dest="filename",
                        default="all", type="string",
                        help="specify filename to encrypt")
 parser.add_option("-p", "--password", dest="password", help="password for lock the arquive")
+parser.add_option("-d", "--decrypt",action="store_false", dest="encrypt", default=True, help="Option for encrypt the filename (is true for default)")
+parser.add_option("-e", "--encrypt", action="store_true", dest="encrypt", help="Option for decrypt the filename")
 
 (options, args) = parser.parse_args()
 if len(args) != 2:
@@ -113,7 +115,16 @@ class Crypto(object):
 
 
 
+try:
+   arquivo = open(filename,"r+b")
+   tamanho = arquivo.tell()
 
-crypto= Crypto(filename, password)
+   print(arquivo)
+   crypto = Crypto(filename, password)
+except IOError:
+   crypto= Crypto(filename, password)
+
+
+
 
 print(crypto)
